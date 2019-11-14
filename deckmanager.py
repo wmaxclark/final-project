67dce78e6212264358900fd874ae8d69416ff092
w_max_clark <31805419+wmaxclark@users.noreply.github.com>
2019-11-14 11:13:35 -0600

    Version 0.0.2
    Added file validaton to study module

---
+++ deckmanager.py
@@ -0,0 +1,4 @@
+def addCards():
+
+
+def addDeck():
--- study.py
+++ study.py
@@ -4,7 +4,7 @@ def study():
 
     import validation as v
     import random as r
-    # import question as q
+    
     userAnswer = ""
     correctAnswer = ""
     cards = []
@@ -18,27 +18,40 @@ def study():
     # userFileRequest = v.getStringByLength("Select a deck to study: ", "Not a valid selection.", 3, 15)
 
     # TODO validate that this file exists
-    deck = open("spanish.txt", "r")
+    # deck = open("C:/Users/Home/Desktop/decks/spanish.txt", "r")
+
+    try:
+        deck = open("C:/Users/Home/Desktop/decks/spanish.txt", "r")
+        # deck = deckFile.readlines() 
+        for card in deck:
+            card = deck.readline()
+            cards.append(card.rstrip("\n"))
+        deck.close()
+    except:
+        print("No saved data. ")
+        salesFile = open("sales.txt", "w")
+        salesFile.close()
 
     
     # Creates an array of all the cards in the deck
     
-    for card in deck:
-        card = deck.readline()
-        cards.append(card.rstrip("\n"))
-    deck.close()
+##    for card in deck:
+##        card = deck.readline()
+##        cards.append(card.rstrip("\n"))
+##    deck.close()
 
     # Loops through 10 times
+    # TODO make this user selectable
+    deckLength = len(cards)
     for i in range(10):
 
         # Picks a random card out of the deck that is acutally in the language that you're studying
         # TODO if possible, select cards weighted by difficulty and how many times you've got them wrong
-        deckLength = len(cards)
-        currentCard = r.randrange(0, deckLength - 1, 2)
+        
+        currentCard = r.randrange(0, len(cards) - 2, 2)
 
         # Repeats as long as you don't have the right answer
-        # TODO keep track of how many you got right/wrong
-        # TODO get input on how hard it was
+        # TODO user feedback and tracked difficulty
         correct = False
         while correct == False:
             userAnswer = v.getStringByLength("What is the English translation of: "  + str(cards[currentCard]) + "\n", "That's not even a word.. ", 0, 30)
