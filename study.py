ce7e47a8b23bcb4242cc7bccf9a9af3d0883ca9b
w_max_clark <31805419+wmaxclark@users.noreply.github.com>
2019-11-14 20:14:28 -0600

    Version 0.0.4
    Added display of existing decks

--- study.py
+++ study.py
@@ -4,10 +4,12 @@ def study():
 
     import validation as v
     import random as r
+    import os
     
     userAnswer = ""
     correctAnswer = ""
-    
+
+    availableDecks = []
     cardPrompts = []
     cardAnswers = []
     cardDifficulty = ""
@@ -16,29 +18,34 @@ def study():
     correct = False
     selecting = True
 
-    # Allows the user type in what deck they want to study
+    
     # TODO display a list of the decks that are already there
-##    userFileRequest = v.getStringByLength("Select a deck to study: ", "Not a valid selection.", 3, 15)
-##    deckPrompts = open("C:/Users/Home/Desktop/decks/" + userFileRequest + "prompts.txt", "r")
+    availableDecks = str(os.listdir("C:/Users/Home/Desktop/decks/"))
+    #availableDecks = availableDecks.name()
+    availableDecks = availableDecks.replace("'", "")
+    availableDecks = availableDecks.replace("[", "")
+    availableDecks = availableDecks.replace("]", "")
 
+    # Allows the user type in what deck they want to study
     
     while selecting == True:
-        userFileRequest = v.getStringByLength("Select a deck to study: ", "Not a valid selection.", 3, 15)
+        print("Please select a deck to study from the following: " + availableDecks)
+        userFileRequest = v.getStringByLength("Type the name here: " , "Not a valid selection.", 3, 15) # Gets valid name of a deck
         try:
-            deckPrompts = open("C:/Users/Home/Desktop/decks/" + userFileRequest + "prompts.txt", "r")
+            deckPrompts = open("C:/Users/Home/Desktop/decks/" + userFileRequest + "/" + userFileRequest + "prompts.txt", "r") # Writes prompts to an array 
             for i in deckPrompts:
                 i = deckPrompts.readline()
                 cardPrompts.append(i.rstrip("\n"))
             deckPrompts.close()
             
-            deckAnswers = open("C:/Users/Home/Desktop/decks/" + userFileRequest + "answers.txt", "r")
+            deckAnswers = open("C:/Users/Home/Desktop/decks/" + userFileRequest + "/" + userFileRequest + "answers.txt", "r") # Writes answers to an array
             for j in deckAnswers:
                 j = deckAnswers.readline()
                 cardAnswers.append(j.rstrip("\n"))
             deckAnswers.close()
             selecting = False
         except:
-            print("No deck for " + userFileRequest + ".") #TODO maybe add an option to create the deck
+            print("No deck for " + userFileRequest + ".") #TODO maybe add an option to create the deck from here
         
 
 
@@ -49,7 +56,7 @@ def study():
 
         # Picks a random card out of the deck that is acutally in the language that you're studying
         # TODO if possible, select cards weighted by difficulty and how many times you've got them wrong
-        
+        # TODO if possible, pick between prompts and answers randomly
         currentCard = r.randrange(0, len(cardPrompts) - 1)
 
         # Repeats as long as you don't have the right answer
