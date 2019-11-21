fd8e76eddb948df5d1263af417da97208d8f209e
w_max_clark <31805419+wmaxclark@users.noreply.github.com>
2019-11-21 11:04:52 -0600

    Version 0.0.9
    Fixing even more crap that I kind of already did in class

--- study.py
+++ study.py
@@ -11,23 +11,19 @@ def study():
     
     userAnswer = ""
     correctAnswer = ""
-    deckPrompts = ""
-    deckAnswers = ""
-    deckDifficulty = ""
+    
     userCardNumber = ""
     
 
     availableDecks = []
-    cardPrompts = []
-    cardAnswers = []
-    cardDifficulty = []
-    currentCardList = []
-    difficultyChanges = []
     deck = []
     card = []
-
+    currentCard = []
+    easyCards = []
+    normalCards = []
+    hardCards = []
     
-    currentCard = 0
+    currentCard = []
     currentDirection = 0
     deckLength = 0
     correct = False
@@ -35,7 +31,7 @@ def study():
 
     
     # Creates an array of strings of the decks that are currently available
-    availableDecks = str(os.listdir("C:/Users/Home/Desktop/decks/"))
+    availableDecks = str(os.listdir("C:/Users/Home/Desktop/decks/")) # TODO THE FILE PATH CHANGES FOR EACH COMPUTER
     availableDecks = availableDecks.replace("'", "")
     availableDecks = availableDecks.replace("[", "")
     availableDecks = availableDecks.replace("]", "")
@@ -57,11 +53,26 @@ def study():
             fh = open("spanish.csv", "r")
             for line in fh:
                 # Strips carriage return
-                line.rstrip("\n")
+                line = line.rstrip("\n")
+                
                 # Splits line into a list called a card
                 card = line.split(",")
+
+                # Adds to easy card list if labeled easy
+                if card[2] == "1":
+                    easyCards.append(card)
+                    
+                # Adds to normal card list if labeled normal
+                elif card[2] == "2":
+                    normalCards.append(card)
+                    
+                # Adds to hard card list if labeled hard
+                elif card[2] == "3":
+                    hardCards.append(card)
+                    
                 # Creates an array of all the cards in the deck
-                deck.append(card)
+                deck.append(easyCards + normalCards + hardCards)
+                
             fh.close()
 
             # When the arrays have all been completed, the user has finished selecting and the loop is closed
@@ -71,56 +82,60 @@ def study():
             print("No deck for " + userFileRequest + ".") #TODO maybe add an option to create the deck from here
 
     
-    
-    print(deck)
+    # TEST
+    # print(deck)
     
 
     # User selects how many cards to study
     userCardNumber = v.getRangedInt("Please enter how many cards you would like to study: ", "Needs to be a number between one and thirty, please. ", 1, 30) # TODO short session medium session or long session instead of user selected
-    userCardNumber = int(userCardNumber)
+    
     for i in range(userCardNumber):
 
         # Randomly picks a direction, either giving prompts and recieving answers or vice versa
         currentDirection = r.randint(0, 1)
 
-        
-
+        currentRange = r.randint(0, 100)
+        if currentRange <= 10 and len(easyCards) > 0:
+            currentCard = easyCards[r.randint(0, len(easyCards))]
+            
+        elif currentRange <= 50 and len(normalCards) > 0:
+            currentCard = normalCards[r.randint(0, len(normalCards))]
+            
+        elif len(hardCards) > 0:
+            currentCard = hardCards[r.randint(0, len(hardCards))]
+    
+            
         # Repeats as long as you don't have the right answer
         # TODO keep track of how many you got right/wrong
-
+        # print(currentCard)
         while correct == False:
-        
-            userAnswer = v.getStringByLength("What is the English translation of: "  + str(cards[currentCard]) + "\n", "That's not even a word.. ", 0, 30)
             
             # Checks direction
             # TODO tracked difficulty
             if currentDirection == 0:
 
                 # Gets an answer from user
-                userAnswer = v.getStringByLength("Prompt: "  + str(cardPrompts[currentCard]) + "\nAnswer: ", "That's not even a word.. \n", 0, 30) # TODO X word and Y word
+                userAnswer = v.getStringByLength("Prompt: "  + str(currentCard[0]) + "\nAnswer: ", "That's not even a word.. \n", 0, 30) # TODO X word and Y word
 
                 # Checks user answer against answer cards
-                if userAnswer == str(cardAnswers[currentCard]):
+                if userAnswer == str(currentCard[1]):
                     print("Correct!\n")
-                    g += 0
-                    currentCard = currentCardList[g]
+                    i += 1
+
+                    
                     correct = True
                 else:
                     print("Not quite, try again\n")
-
                 
-
-
             elif currentDirection == 1:
 
                 # Gets an answer from user
-                userAnswer = v.getStringByLength("Answer: "  + str(cardAnswers[currentCard]) + "\nPrompt: ", "That's not even a word.. \n", 0, 30) # TODO X word Y word
+                userAnswer = v.getStringByLength("Answer: "  + str(currentCard[1]) + "\nPrompt: ", "That's not even a word.. \n", 0, 30) # TODO X word Y word
 
                 # Checks user answer against prompt cards as the direction is reversed
-                if userAnswer == str(cardPrompts[currentCard]):
+                if userAnswer == str(currentCard[0]):
                     print("Correct!\n")
-                    g += 0
-                    currentCard = currentCardList[g]
+                    i += 1
                     correct = True
                 else:
                     print("Not quite, try again\n")
